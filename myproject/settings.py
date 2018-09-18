b199972a8fcf4e61888e489d153a101f610b410a
Bokeum <bokeum.eom@gmail.com>
2018-09-18 18:12:33 +0900

    Django first board commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
+    'accounts',
     'boards',
 ]
 
@@ -123,3 +125,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
